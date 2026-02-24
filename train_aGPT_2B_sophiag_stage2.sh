81ac9c58374d21528879f6cae6e249b9bfa09d87
Sam Foreman <saforem2@gmail.com>
2026-02-24 09:45:08 -0600

    feat: Add `train_aGPT_2B_sophiag_stage2.sh`

---
+++ train_aGPT_2B_sophiag_stage2.sh
@@ -0,0 +1,41 @@
+#!/bin/bash --login
+#PBS -q prod
+#PBS -A AuroraGPT
+#PBS -j oe
+#PBS -l walltime=06:00:00,filesystems=flare:home
+#PBS -l select=256
+
+setup_env() {
+	cd "${PBS_O_WORKDIR}" || {
+		echo "Failed to change directory to ${PBS_O_WORKDIR}"
+		exit 1
+	}
+	# shellcheck disable=SC1090
+	source <(curl -L https://bit.ly/ezpz-utils)
+	ezpz_setup_env
+	log_message INFO "Using: $(which python3)"
+}
+
+train_model() {
+	MODEL_ARCH=AuroraGPT-2B \
+		TRAIN_TOKENS=7064155541716 \
+		OPT=sophiag \
+		LR=2.17e-5 \
+		GRAD_ACC_STEPS=2 \
+		MICRO_BATCH=1 \
+		USE_ACTIVATION_CHECKPOINTING=0 \
+		ZERO_STAGE=0 \
+		LR_DECAY_STYLE=constant \
+		TOKENIZER_TYPE=HFTokenizer \
+		TOKENIZER_MODEL=google/gemma-7b \
+		DATA_FILE_LIST=ALCF/data-lists/aurora/dolmino-mix-1124-fused-file-list.txt \
+		bash "${PBS_O_WORKDIR}/train_alcf.sh" \
+		"$@"
+}
+
+main() {
+	setup_env
+	train_model "$@"
+}
+
+main "$@"
